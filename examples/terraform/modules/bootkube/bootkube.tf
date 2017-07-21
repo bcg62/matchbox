4ac904736f8a180bbdf5cb120f5f6854844522c4
Dalton Hubble <dghubble@gmail.com>
2017-07-20 17:11:57 -0700

    Draft: Update Kubernetes to v1.7.1

--- examples/terraform/modules/bootkube/bootkube.tf
+++ examples/terraform/modules/bootkube/bootkube.tf
@@ -1,6 +1,6 @@
 # Self-hosted Kubernetes assets (kubeconfig, manifests)
 module "bootkube" {
-  source = "git::https://github.com/dghubble/bootkube-terraform.git?ref=v0.5.1"
+  source = "git::https://github.com/dghubble/bootkube-terraform.git?ref=4d0ce629d1d97f1814d6d4733e9b0da036f948e1"
 
   cluster_name                  = "${var.cluster_name}"
   api_servers                   = ["${var.k8s_domain_name}"]
